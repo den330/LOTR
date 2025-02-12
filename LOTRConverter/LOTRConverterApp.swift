4421a2a32eba58eaf3d662f5630fff9a32bdc257
den330 <wo131485508@qq.com>
2025-02-12 12:07:57 -0500

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by yaxin on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
